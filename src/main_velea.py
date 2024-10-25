55e922001410beaa4a11d6579beadadcd06c4b0f
Martin Neumayer <martin.neumayer@th-rosenheim.de>
2024-10-25 14:26:40 +0200

    Reset sliver_threshold to 100 m^2

--- src/main_velea.py
+++ src/main_velea.py
@@ -20,7 +20,7 @@ def run():
         base_area,
         includes,
         excludes,
-        sliver_threshold=0,
+        sliver_threshold=100,
         crs="EPSG:25832",
     ).execute()
 
@@ -31,13 +31,6 @@ print(f"Runtimes of VELEA:")
 print(timer)
 print(f"Minimum runtime of VELEA: {min(timer):.2f}")
 
-eligible_areas, restricted_areas = EligibilityAnalysis(
-    base_area,
-    includes,
-    excludes,
-    restricted,
-    sliver_threshold=0,
-    crs="EPSG:25832",
-).execute()
+eligible_areas, restricted_areas = run()
 eligible_areas.to_file(f"{output_directory}/VELEA-eligible.gpkg")
 restricted_areas.to_file(f"{output_directory}/VELEA-restricted.gpkg")
