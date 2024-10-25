afd20aad41ec96e38835ded5ac4a123b560e11f7
Martin Neumayer <martin.neumayer@th-rosenheim.de>
2024-10-25 14:17:45 +0200

    Fix crs in main_glaes.py, add pruning eligible areas < 100 m^2

--- src/main_glaes.py
+++ src/main_glaes.py
@@ -9,22 +9,29 @@ number_of_repetitions = 10
 
 
 def run():
-    ec = ExclusionCalculator(region_path, srs=3035, pixelRes=pixel_resolution, initialValue=False)
+    ec = ExclusionCalculator(
+        region_path, srs=25832, pixelRes=pixel_resolution, initialValue=False
+    )
     for include in includes:
         ec.excludeVectorType(**include)
     for exclude in excludes:
         ec.excludeVectorType(**exclude)
+    ec.pruneIsolatedAreas(minSize=100)
     return ec
 
 
 for pixel_resolution in pixel_resolutions:
-    print(f'Running GLAES {number_of_repetitions} times with a pixel resolution of {pixel_resolution}')
+    print(
+        f"Running GLAES {number_of_repetitions} times with a pixel resolution of {pixel_resolution}"
+    )
     timer = timeit.Timer(run).repeat(number=1, repeat=number_of_repetitions)
-    print(f'Runtimes of GLAES with pixel resolution of {pixel_resolution}:')
+    print(f"Runtimes of GLAES with pixel resolution of {pixel_resolution}:")
     print(timer)
-    print(f'Minimum runtime of GLAES with pixel resolution of {pixel_resolution}: {min(timer):.2f}')
+    print(
+        f"Minimum runtime of GLAES with pixel resolution of {pixel_resolution}: {min(timer):.2f}"
+    )
 
     exclusion_calculator = run()
-    exclusion_calculator.save(f'{output_directory}/GLAES_{pixel_resolution}.tif')
+    exclusion_calculator.save(f"{output_directory}/GLAES_{pixel_resolution}.tif")
 
     # exclusion_calculator.draw()
